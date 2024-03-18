c6d667c7e81e1c63d43e19bd1f9b3b92e525d7ca
PavelSchlegel <pavel.pinegin.essen@gmail.com>
2024-03-18 21:22:17 +0100

    first_commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(880, 640), "My window");
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {   
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+
+        window.display();
+    }
+    return 0;
+}
