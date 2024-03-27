d754ac8b319ee4bbafc13483e43a0b78a527e9d2
pinegin pavel <pavel.pinegin.essen@gmail.com>
2024-03-27 18:10:52 +0100

    buttonArray

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@
 class Ob_Button
 {
 public:
+
+    bool m_flag;
+    uint8_t m_field;
     sf::Texture m_texture;
     sf::Sprite m_sprite;
 
-    Ob_Button()
+    Ob_Button(uint8_t value, float x, float y) :
+    m_flag(false),
+    m_field()
+
     {
         if ( ! m_texture.loadFromFile("../../GPG/PlayField.jpg"/*, sf::IntRect(320, 0, 32, 32)*/)) {
             std::cout << "Error-Image was not open!" << std::endl;
@@ -23,18 +29,41 @@ public:
         return m_sprite;
     }
 
-    void buttonOne() {
-        m_sprite.setTextureRect(sf::IntRect(288, 0, 32, 32));
+    void buttonOpen() {
+        if ( ! m_flag ) {
+            m_sprite.setTextureRect(sf::IntRect(m_field * 32, 0, 32, 32));
+        }
+    }
+
+    void buttonFlag() {
+        if ( ! m_flag ) {
+            m_sprite.setTextureRect(sf::IntRect(352, 0, 32, 32));
+            return;
+        }
+        m_sprite.setTextureRect(sf::IntRect(320, 0, 32, 32));
     }
 };
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(880, 640), "My window");
+    window.setVerticalSyncEnabled(true);
     sf::Clock clock;
 
     int w = 32;
-    Ob_Button freeButton;
+    // Ob_Button freeButton;
+    Ob_Button field[10][10];
+    for (std::size_t i = 0; i < 10; ++i) {
+        for (std::size_t j = 0; j < 10; ++j) {
+            try {
+                field[i][j].get_sprite().setPosition(i * w, j * w);
+                // window.draw(field[i][j].get_sprite());
+            }
+            catch (...) {
+                std::cout << "Try/catch-error!" << std::endl;
+            }
+        }
+    }
 
     while (window.isOpen())
     {
@@ -47,23 +76,53 @@ int main()
         }
 
         window.clear(sf::Color::White);
-        freeButton.get_sprite().setPosition(/*x*/32, /*y*/32);
+
+        //freeButton.get_sprite().setPosition(/*x*/32, /*y*/32);
 
         if (event.type == sf::Event::MouseButtonPressed) {
             if (event.mouseButton.button == sf::Mouse::Left) {
-                if (event.mouseButton.x >= freeButton.get_sprite().getPosition().x) {
-                    if (event.mouseButton.x <= freeButton.get_sprite().getPosition().x + w) {
-                        if (event.mouseButton.y >= freeButton.get_sprite().getPosition().x) {
-                            if (event.mouseButton.y <= freeButton.get_sprite().getPosition().x + w) {
-                                freeButton.buttonOne();
-                            }
-                        }
-                    }
+                // if (event.mouseButton.x >= freeButton.get_sprite().getPosition().x) {
+                //     if (event.mouseButton.x <= freeButton.get_sprite().getPosition().x + w) {
+                //         if (event.mouseButton.y >= freeButton.get_sprite().getPosition().x) {
+                //             if (event.mouseButton.y <= freeButton.get_sprite().getPosition().x + w) {
+                //                freeButton.buttonOne();
+                //             }
+                //         }
+                //     }
+                // }
+                std::cout << event.mouseButton.x / w << " " <<  event.mouseButton.y / w << std::endl;
+                if ((event.mouseButton.x / w) < 10) {
+                    if ((event.mouseButton.y / w) < 10)
+                    field[event.mouseButton.x / w][event.mouseButton.y / w].buttonOpen();
+                } else {
+                    std::cout << "Mous-field_ERROR!" << std::endl;
+                }
+            }
+
+            if (event.mouseButton.button == sf::Mouse::Right) {
+                std::cout << event.mouseButton.x / w << " " <<  event.mouseButton.y / w << std::endl;
+                if ((event.mouseButton.x / w) < 10) {
+                    if ((event.mouseButton.y / w) < 10)
+                    field[event.mouseButton.x / w][event.mouseButton.y / w].buttonFlag();
+                } else {
+                    std::cout << "Mous-field_ERROR!" << std::endl;
+                }
+            }
+        }
+
+        for (std::size_t i = 0; i < 10; ++i) {
+            for (std::size_t j = 0; j < 10; ++j) {
+                try {
+                    // field[i][j].get_sprite().setPosition(i * w, j * w);
+                    window.draw(field[i][j].get_sprite());
+                }
+                catch (...) {
+                    std::cout << "Try/catch-error!" << std::endl;
                 }
             }
         }
         
-        window.draw(freeButton.get_sprite());
+        //window.draw(freeButton.get_sprite());
 
         window.display();
     }
