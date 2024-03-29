e1d739e5ea9286b97af81961ffffec7a243f7a0e
pinegin pavel <pavel.pinegin.essen@gmail.com>
2024-03-29 23:08:09 +0100

    mine...

--- main.cpp
+++ main.cpp
@@ -7,57 +7,94 @@ class Ob_Button
 public:
 
     bool m_flag;
-    uint8_t m_field;
+    bool is_open;
+    uint8_t m_fieldValue;
     sf::Texture m_texture;
     sf::Sprite m_sprite;
 
-    Ob_Button(uint8_t value, float x, float y) :
-    m_flag(false),
-    m_field()
+    Ob_Button () :
+    m_flag ( false ),
+    is_open ( false ),
+    m_fieldValue ( 0 )
 
     {
-        if ( ! m_texture.loadFromFile("../../GPG/PlayField.jpg"/*, sf::IntRect(320, 0, 32, 32)*/)) {
+        if ( ! m_texture .loadFromFile ( "../../GPG/PlayField.jpg" ) ) {
             std::cout << "Error-Image was not open!" << std::endl;
         }
         else {
-            m_sprite.setTexture(m_texture);
-            m_sprite.setTextureRect(sf::IntRect(320, 0, 32, 32));
+            m_sprite .setTexture ( m_texture );
+            m_sprite .setTextureRect ( sf::IntRect ( 320, 0, 32, 32) );
         }
     }
 
-    sf::Sprite& get_sprite() {
-        return m_sprite;
+    explicit Ob_Button ( uint8_t value, float x, float y ) :
+    m_flag ( false ),
+    is_open ( false ),
+    m_fieldValue ( value )
+
+    {
+        if ( ! m_texture .loadFromFile ( "../../GPG/PlayField.jpg" ) ) {
+            std::cout << "Error-Image was not open!" << std::endl;
+        }
+        else {
+            m_sprite .setTexture ( m_texture );
+            m_sprite .setTextureRect ( sf::IntRect ( 320, 0, 32, 32 ) );
+            m_sprite .setPosition ( x, y ) ;
+        }
     }
 
-    void buttonOpen() {
+    void set_value ( uint8_t value ) {
+
+        if ( value < 10 ) {
+            m_fieldValue = value;
+        }
+    }
+
+    void buttonOpen () {
         if ( ! m_flag ) {
-            m_sprite.setTextureRect(sf::IntRect(m_field * 32, 0, 32, 32));
+            if ( ! is_open ) {
+                is_open = true;
+                m_sprite .setTextureRect ( sf::IntRect ( m_fieldValue * 32, 0, 32, 32 ) );
+            }
         }
     }
 
-    void buttonFlag() {
-        if ( ! m_flag ) {
-            m_sprite.setTextureRect(sf::IntRect(352, 0, 32, 32));
-            return;
+    void buttonFlag () {
+        if ( ! is_open ) {
+            if ( ! m_flag ) {
+                m_flag = true;
+                m_sprite .setTextureRect ( sf::IntRect ( 352, 0, 32, 32 ) );
+                return;
+            }
+            m_flag = false;
+            m_sprite .setTextureRect ( sf::IntRect ( 320, 0, 32, 32 ) );
         }
-        m_sprite.setTextureRect(sf::IntRect(320, 0, 32, 32));
     }
 };
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(880, 640), "My window");
-    window.setVerticalSyncEnabled(true);
+    sf::RenderWindow window ( sf::VideoMode ( 880, 640 ), "My window" );
+    // window.setVerticalSyncEnabled(true);
+    window .setFramerateLimit ( 30 );
     sf::Clock clock;
 
-    int w = 32;
-    // Ob_Button freeButton;
-    Ob_Button field[10][10];
-    for (std::size_t i = 0; i < 10; ++i) {
-        for (std::size_t j = 0; j < 10; ++j) {
+    bool ButtonRight = false;
+
+    std::size_t w = 32;
+
+    Ob_Button field [ 10 ] [ 10 ];
+    for ( std::size_t i = 0; i < 10; ++i ) {
+        for ( std::size_t j = 0; j < 10; ++j ) {
             try {
-                field[i][j].get_sprite().setPosition(i * w, j * w);
-                // window.draw(field[i][j].get_sprite());
+                field [ i ] [ j ] .m_sprite .setPosition ( static_cast < float > ( i * w ), static_cast < float > ( j * w ) );
+                // difficulty level ???
+                if ( rand () % 5 == 0 ) {
+                    field [ i ] [ j ] .set_value ( 9 );
+                }
+			    else {
+                    field [ i ] [ j ] .set_value ( 0 );
+                }
             }
             catch (...) {
                 std::cout << "Try/catch-error!" << std::endl;
@@ -65,66 +102,59 @@ int main()
         }
     }
 
-    while (window.isOpen())
+    while ( window .isOpen () )
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while ( window .pollEvent ( event ) )
         {   
-            if (event.type == sf::Event::Closed){
-                window.close();
+            if ( event.type == sf::Event::Closed ){
+                window .close ();
             }
         }
 
-        window.clear(sf::Color::White);
+        window .clear( sf::Color::White );
 
-        //freeButton.get_sprite().setPosition(/*x*/32, /*y*/32);
-
-        if (event.type == sf::Event::MouseButtonPressed) {
-            if (event.mouseButton.button == sf::Mouse::Left) {
-                // if (event.mouseButton.x >= freeButton.get_sprite().getPosition().x) {
-                //     if (event.mouseButton.x <= freeButton.get_sprite().getPosition().x + w) {
-                //         if (event.mouseButton.y >= freeButton.get_sprite().getPosition().x) {
-                //             if (event.mouseButton.y <= freeButton.get_sprite().getPosition().x + w) {
-                //                freeButton.buttonOne();
-                //             }
-                //         }
-                //     }
-                // }
-                std::cout << event.mouseButton.x / w << " " <<  event.mouseButton.y / w << std::endl;
-                if ((event.mouseButton.x / w) < 10) {
-                    if ((event.mouseButton.y / w) < 10)
-                    field[event.mouseButton.x / w][event.mouseButton.y / w].buttonOpen();
-                } else {
-                    std::cout << "Mous-field_ERROR!" << std::endl;
+        if ( event.type == sf::Event::MouseButtonPressed ) {
+            std::cout << " Mouse active " << std::endl;
+            if ( event.key.code == sf::Mouse::Left ) {
+                if ( ( event.mouseButton.x / w ) < 10 && ( event.mouseButton.y / w ) < 10 ) {
+                    if ( ! field [ event.mouseButton.x / w ] [ event.mouseButton.y / w ] .is_open ) {
+                        field [ event.mouseButton.x / w ] [ event.mouseButton.y / w ] .buttonOpen ();
+                    }
                 }
+                std::cout << " Mouse left_button " << std::endl;
             }
 
-            if (event.mouseButton.button == sf::Mouse::Right) {
-                std::cout << event.mouseButton.x / w << " " <<  event.mouseButton.y / w << std::endl;
-                if ((event.mouseButton.x / w) < 10) {
-                    if ((event.mouseButton.y / w) < 10)
-                    field[event.mouseButton.x / w][event.mouseButton.y / w].buttonFlag();
-                } else {
-                    std::cout << "Mous-field_ERROR!" << std::endl;
-                }
+            if ( event.key.code == sf::Mouse::Right ) {
+                std::cout << " Mouse right_button " << std::endl;
+                ButtonRight = true;
             }
         }
 
-        for (std::size_t i = 0; i < 10; ++i) {
-            for (std::size_t j = 0; j < 10; ++j) {
+        for ( std::size_t i = 0; i < 10; ++i ) {
+            for ( std::size_t j = 0; j < 10; ++j ) {
                 try {
-                    // field[i][j].get_sprite().setPosition(i * w, j * w);
-                    window.draw(field[i][j].get_sprite());
+                    window .draw ( field [ i ] [ j ] .m_sprite );
                 }
                 catch (...) {
                     std::cout << "Try/catch-error!" << std::endl;
                 }
             }
         }
-        
-        //window.draw(freeButton.get_sprite());
 
-        window.display();
+        if ( ButtonRight ) {
+            if ( event.type == sf::Event::MouseButtonReleased ) {
+                std::cout << " Mouse right_release " << std::endl;
+                if ( event.key.code == sf::Mouse::Right/* sf::Mouse::isButtonPressed(sf::Mouse::Button::Right )*/ ) {
+                    if ( ( event.mouseButton.x / w ) < 10 && ( event.mouseButton.y / w ) < 10 ) {
+                        field [ event.mouseButton.x / w ] [ event.mouseButton.y / w ] .buttonFlag ();
+                        ButtonRight = false;
+                    }
+                }
+            }
+        }
+
+        window .display ();
     }
     return 0;
 }
